9b9f4409b233734942a7dedf6e729ba9a6371711
Pranavvks <pranav.vks@gmail.com>
2021-01-23 09:34:57 +0530

    Changed the HTML for the site

---
+++ blog/urls.py
@@ -0,0 +1,6 @@
+from django.urls import path
+from .import views
+urlpatterns = [ 
+    path('',views.post_list , name='post_list') ,
+]
+# Here we are importing Django's function path and all of our views from the blog application.
